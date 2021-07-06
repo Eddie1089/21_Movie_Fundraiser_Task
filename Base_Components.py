ec142d5ed7b433147362141eb0f5b7f97fe16d72
Eddie1089 <tabene.edwar@awatapu.school.nz>
2021-07-07 11:53:31 +1200

    Initial commit

---
+++ Base_Components.py
@@ -0,0 +1,31 @@
+# Import Statements
+print ()
+# Functions go here
+
+# ********** Main Routine **********
+
+# Set up dictionary's/lists to hold data
+
+# Ask the user if they have used to program before and give instructions if necessary
+
+# Loop to get ticket details
+
+    # Get name (can't be blank)
+
+    # Get age (between 12 and 130)
+
+    # Calculate ticket price
+
+    # Loop to ask for snacks
+
+    # Calculate snack price
+
+    # Ask for payment method (and apply surcharge if necessary (5%))
+
+# Calculate sales and profit
+
+# Output to CSV file
+
+
+
+
