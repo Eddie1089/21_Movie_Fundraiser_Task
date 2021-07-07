c2094ea45ddecf127c315adf5c1273935c94f7b9
Eddie1089 <tabene.edwar@awatapu.school.nz>
2021-07-07 12:25:05 +1200

    Added not blank function

--- 01_not_blank_function.py
+++ 01_not_blank_function.py
@@ -1,3 +1,15 @@
 # function goes here
 
-# *** Main Routine gores here ***
+def not_blank(question):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        if response != "":
+            return response
+        else:
+            print("This field can NOT be blank please enter your name")
+
+
+name = not_blank("Name: ")
