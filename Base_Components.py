e87cb26191cbe9163158bd4061557bcfda954c80
Eddie1089 <tabene.edwar@awatapu.school.nz>
2021-07-30 14:04:48 +1200

    Added "seat(s)"

--- Base_Components.py
+++ Base_Components.py
@@ -3,6 +3,43 @@ print ()
 # Functions go here
 
 # ********** Main Routine **********
+def not_blank(question, error_message):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        if response != "":
+            return response
+        else:
+            print(error_message)
+
+
+# Main Routine goes here
+name = not_blank("Name: ",
+                 "This field can NOT be blank, "
+                 "please enter your name")
+
+name = ""
+count = 0
+MAX_TICKETS = 5
+
+while name != "xxx" and count < MAX_TICKETS:
+    print("You have {} seat(s) "
+          "left".format(MAX_TICKETS - count))
+
+    #   Get details...
+    name = input("Name: ")
+    count += 1
+    print()
+
+if count == MAX_TICKETS:
+    print("You have sold all of the available tickets!")
+else:
+    print("You have sold {} tickets.    \n"
+          "There are {} places still available"
+          .format(count, MAX_TICKETS - count))
+
 
 # Set up dictionary's/lists to hold data
 
