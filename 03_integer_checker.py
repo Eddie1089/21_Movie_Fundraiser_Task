4187eac6afd3b46223d608eb39d5d30a0aefed42
Eddie1089 <tabene.edwar@awatapu.school.nz>
2021-08-02 10:09:49 +1200

    Added "seat()"

---
+++ 03_integer_checker.py
@@ -0,0 +1,23 @@
+def int_check(question, low_num, high_num):
+
+    error = "Please enter a whole number between {} " \
+            "and {}".format(low_num, high_num)
+
+    vaild = False
+    while not vaild:
+
+            # ask the user for a number and check it is valid
+            try:
+                response = int(input(question))
+
+                if low_num <= response <= high_num:
+                    return response
+                else:
+                    print(error)
+
+            # if an integer is not entered, display an error
+            except ValueError:
+                print(error)
+
+age = int_check("Age: ", 12, 130)
+
