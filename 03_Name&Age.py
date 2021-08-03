bc7c11c997394049d3a6eb5c33a5c7ed975d05d4
Eddie1089 <tabene.edwar@awatapu.school.nz>
2021-08-03 14:24:57 +1200

    Works now

--- 03_Name&Age.py
+++ 03_Name&Age.py
@@ -1,33 +1,17 @@
-def not_blank(question, error_message):
-    valid = False
-
-    while not valid:
-        response = input(question)
-
-        if response != "":
-            return response
-        else:
-            print(error_message)
-
-
-# Main Routine goes here
-name = not_blank("Name: ",
-                 "This field can NOT be blank, "
-                 "please enter your name")
-
 def int_check(question, low_num, high_num):
 
     error = "Please enter a whole number between {} " \
             "and {}".format(low_num, high_num)
 
-    vaild = False
-    while not vaild:
+    valid = False
+    while not valid:
 
             # ask the user for a number and check it is valid
+
             try:
                 response = int(input(question))
 
-                if low_num <= response <= high_num:
+                if low_num < response < high_num:
                     return response
                 else:
                     print(error)
@@ -36,6 +20,27 @@ def int_check(question, low_num, high_num):
             except ValueError:
                 print(error)
 
-age = int_check("Age: ", 12, 130)
 
+name = ""
+count = 0
+MAX_TICKETS = 5
 
+while name != "xxx" and count < MAX_TICKETS:
+    print("You have {} seat(s) "
+          "left".format(MAX_TICKETS - count))
+
+    #   Get details...
+    name = input("Name: ")
+
+    if name == "xxx":
+        break
+    age = int_check("Age: ", 11, 131)
+    count += 1
+    print()
+
+    if count == MAX_TICKETS:
+        print("You have sold all of the available tickets!")
+    else:
+        print("You have sold {} tickets.    \n"
+              "There are {} places still available"
+              .format(count, MAX_TICKETS - count))
