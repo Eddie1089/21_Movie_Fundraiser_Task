797e0c31daf4b0cb0bda4faddb18a059a1979940
Eddie1089 <tabene.edwar@awatapu.school.nz>
2021-08-03 13:39:14 +1200

    Added "seat()"

--- Base_Components.py
+++ Base_Components.py
@@ -1,19 +1,27 @@
 # Import Statements
 
 # Functions go here
-def not_blank(question):
+
+# Checks that the name on the ticket is not blank
+def not_blank(question, error_message):
     valid = False
 
     while not valid:
         response = input(question)
 
+        # If name is not blank, program continues
         if response != "":
             return response
-        else:
-            print("This field can NOT be blank,"
-                  "please enter you name.")
-# Checks that the name on the ticket is not blank
 
+        # if  name is blank show error, and repeat loop
+        else:
+            print(error_message)
+
+
+# Main Routine goes here
+name = not_blank("Name: ",
+                 "This field can NOT be blank, "
+                 "please enter your name")
 # ********** Main Routine **********
 
 # Main Routine goes here
@@ -25,7 +33,6 @@ def not_blank(question):
 # Loop to get ticket details
 
     # Get name (can't be blank)
-    name = not_blank("Name: ")
 
     # Get age (between 12 and 130)
 
