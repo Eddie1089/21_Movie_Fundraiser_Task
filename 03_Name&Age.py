07e9982b8b7c15663d119e8bd0820ddb1e4c0168
Eddie1089 <tabene.edwar@awatapu.school.nz>
2021-08-04 12:28:43 +1200

    Working com code

--- 03_Name&Age.py
+++ 03_Name&Age.py
@@ -1,42 +1,46 @@
-def int_check(question, low_num, high_num):
-
-    error = "Please enter a whole number between {} " \
-            "and {}".format(low_num, high_num)
+def int_check(question):
+    error = "Please enter a whole number between 12 amd 130."
 
     valid = False
     while not valid:
 
-            # ask the user for a number and check it is valid
+        # ask the user for a number and check it is valid
+        try:
+            response = int(input(question))
 
-            try:
-                response = int(input(question))
-
-                if low_num < response < high_num:
-                    return response
-                else:
-                    print(error)
-
-            # if an integer is not entered, display an error
-            except ValueError:
+            if response <=0:
                 print(error)
+            else:
+                return response
 
-
+        except ValueError:
+            print(error)
 name = ""
 count = 0
 MAX_TICKETS = 5
 
 while name != "xxx" and count < MAX_TICKETS:
-    print("You have {} seat(s) "
-          "left".format(MAX_TICKETS - count))
+    print("You have {} seat(s) left".format(MAX_TICKETS - count))
 
     #   Get details...
     name = input("Name: ")
-
     if name == "xxx":
         break
-    age = int_check("Age: ", 11, 131)
+
+    age = int_check("Age: ")
+
+    if age < 12:
+        print("You are two young to watch this film!")
+        continue
+    elif age > 130:
+        print("You are too old to watch this film, or this input was a mistake!")
+        continue
+
     count += 1
+
+
     print()
+
 
     if count == MAX_TICKETS:
         print("You have sold all of the available tickets!")
